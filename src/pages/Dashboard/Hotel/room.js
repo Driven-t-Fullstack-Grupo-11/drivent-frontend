1d4a5ff192b5172797e5a2850c547c027f5e6c70
Lorena <blorena1321@gmail.com>
2023-08-04 15:24:30 -0300

    hotel finalizado

---
+++ src/pages/Dashboard/Hotel/room.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export default function Room() {
+  return (
+    <>
+      <RoomCard>testando</RoomCard>
+    </>
+  );
+}
+
+const RoomCard = styled.div`
+  background-color: pink;
+  padding: 5px;
+  border-radius: 10px;
+`;
